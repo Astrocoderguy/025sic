d2c6dfaaaa520f90f66f974d66d446a19ac5f68c
Matthew Olson <impala.cis.ksu.edu>
2013-09-13 12:20:42 -0500

    Added Semaphore and Semaphore init to thread

--- cis520/pintos/src/threads/thread.h
+++ cis520/pintos/src/threads/thread.h
@@ -83,15 +83,16 @@ typedef int tid_t;
 struct thread
   {
     /* Owned by thread.c. */
-    tid_t tid;                          /* Thread identifier. */
-    enum thread_status status;          /* Thread state. */
-    char name[16];                      /* Name (for debugging purposes). */
-    uint8_t *stack;                     /* Saved stack pointer. */
-    int priority;                       /* Priority. */
+    tid_t tid;                          /* Thread identifier.                 */
+    enum thread_status status;          /* Thread state.                      */
+    char name[16];                      /* Name (for debugging purposes).     */
+    uint8_t *stack;                     /* Saved stack pointer.               */
+    int priority;                       /* Priority.                          */
     struct list_elem allelem;           /* List element for all threads list. */
+    semaphore s;                        /* Semaphore variable                 */
 
     /* Shared between thread.c and synch.c. */
-    struct list_elem elem;              /* List element. */
+    struct list_elem elem;              /* List element.                      */
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
