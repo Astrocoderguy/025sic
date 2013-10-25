387736aaf633edea4a962ada6db2936b6f036c52
Matt <m69741@k-state.edu>
2013-10-24 23:33:20 -0500

    Some system call stuff

--- cis520/pintos/src/userprog/syscall.c
+++ cis520/pintos/src/userprog/syscall.c
@@ -42,13 +42,55 @@ syscall_init (void)
   lock_init (&fs_lock);
 }
  
-/* System call handler. */
+typedef int syscall_function( int, int, int );
+struct syscall
+{
+  size_t arg_cnt;           /* Number of arguements */
+  syscall_function *func;   /* Implementation       */
+}
 
+static const struct syscall syscall_table[] =
+{
+  {0, (syscall_function * ) sys_halt},
+  {1, (syscall_function * ) sys_exit},
+  {1, (syscall_function * ) sys_exec},
+  {1, (syscall_function * ) sys_wait},
+  {2, (syscall_function * ) sys_create},
+  {1, (syscall_function * ) sys_remove},
+  {1, (syscall_function * ) sys_open},
+  {1, (syscall_function * ) sys_filesize},
+  {3, (syscall_function * ) sys_read},
+  {3, (syscall_function * ) sys_write},
+  {2, (syscall_function * ) sys_seek},
+  {1, (syscall_function * ) sys_tell},
+  {1, (syscall_function * ) sys_close}
+}
+
+/* System call handler. */
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
+  const struct syscall *sc;
+  unsigned call_nr;
+  int args[3];
+
+  /* Get the system call */
+  copy_in( &call_nr, f->esp, sizeof(call_nr) );
+  if( call_nr >= sizeof(sycall_table) / sizeof(*sycall_table) ) 
+    thread_exit();
+  sc = syscall_table + call_nr;
+
+  /* Get the system call arguments */
+  ASSERT( sc->arg_cnt <= sizeof( args) / sizeof( *args ) );
+  memset( args, 0, sizeof( args ));
+  copy_in( args, (uint23_t *) f->esp + 1, sizeof( *args * sc->arg_cnt ) );
+
+  /* Execute system call and set return value */
+  f->aex = sc->func( args[0], args[1], args[2] );
+/*
   printf ("system call!\n");
   thread_exit ();
+*/
 }
 
 /* Returns true if UADDR is a valid, mapped user address,
